02504bfad968ce39eaccde76994a2453a549a658
AshwathMittal <ashwathmittal@gmail.com>
2021-10-01 13:45:04 +0530

    Removed Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HomeComponent } from './comp/home/home.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
