eb71a1bc3b4b30a208885e74ad3cb3fff876be54
AshwathMittal <ashwathmittal@gmail.com>
2021-12-25 13:13:39 +0530

    Fixed Bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HashLocationStrategy, LocationStrategy  } from '@angular/common';
+// import { HashLocationStrategy, LocationStrategy  } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './comp/home/home.component';
@@ -25,7 +25,7 @@ import { LSkillsComponent } from './linux/l-skills/l-skills.component';
     AppRoutingModule
   ],
   providers: [
-    {provide : LocationStrategy , useClass: HashLocationStrategy}
+    // {provide : LocationStrategy , useClass: HashLocationStrategy}
 
   ],
   bootstrap: [AppComponent]
--- src/projects.ts
+++ src/projects.ts
@@ -10,31 +10,29 @@ export const projects = [
       {
         title: "Hoodie Mart",
         desc: "This is an E-commerce website which was made for an Interschool Competition. It was supposed to be just a Frontend Website therefor it doesn't have much functionality, but the design is great. We used React Framework to build this.",
-        src: "assets/shop.gif",
         href: "https://simple-e-market.netlify.app/"
       },
       {
         title: "ChatBot",
         desc: 'This is a prototype of a chatBot made in HTML, CSS & Js. This is just a tempelate, therefore the bot does not reply anything except "Hello!"',
-        src: "assets/chatbot.gif",
+
         href: "https://ashwathmittal.github.io/first-chat-bot/"
       },      
       {
         title: "Chatbot with machine Learning",
         desc: "The GUI of this bot is same as the above Bot, the differnce is that this is uses machine learning to reply. The backend where the model training takes place is made in NodeJS framework using Brain.js Library.",
-        src: "assets/aiwaala.gif",
         href: "https://github.com/AshwathMittal/AI-chat-bot"
       },
       {
         title: "To Do List App",
         desc: "This is my first angular app, I made this while learning angular. In this WebApp you can store your to do list. The To-Do items are stored in the browser's local storage, so you won't loose the saved items when the page is refreshed.",
-        src: "assets/todolist.gif",
+
         href: "https://ashwathmittal.github.io/ListToDo/"
       },
       {
         title: "Diary Entry App",
         desc: "This is a simple diary entry app made with HTML, CSS & Ts using Angular CLI. It is a simple app which allows you to write your diary entries in a simple way.",
-        src: "assets/diary.png",
+      
         href: "https://ashwathmittal.github.io/De-Diary/"
       },
       // {
@@ -46,14 +44,11 @@ export const projects = [
       {
         title: "Also Check My GiHub For Other Projects",
         desc: "",
-        src: "assets/github.png",
         href: "https://github.com/AshwathMittal"
       },
 
       {
         title: "More Projects To Be Uploaded Soon...",
         desc: null,
-        src: "assets/comingsoon.gif",
-        href: "assets/comingsoon.gif"
       }
 ]
