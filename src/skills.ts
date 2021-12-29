b0cec6f4ee982cac5af30a3602ef8b6f775178f3
AshwathMittal <ashwathmittal@gmail.com>
2021-12-29 13:41:48 +0530

    Grammar Fixes

--- src/skills.ts
+++ src/skills.ts
@@ -1,14 +1,14 @@
-   export const skills =  [
-            {
-              name: "Web Development",
-              desc: `  I am a Frontend-Developer and I am well experienced with HTML, CSS, Javascript, TypeScript, Angular, NodeJS, Express, MongoDB.
-              Though I know a few Backend technologies I choose to be a Frontend-Developer.`
-            },
-            {
-              name: "Python",
-              desc: `
-              I am a Python Learner with an Intermidiate experience with Python.
-              I have a little bit knowledge of Machine Learning and Deep Learning.
-              `
-            }
-          ]
+export const skills = [
+  {
+    name: 'Web Development',
+    desc: `  I am a Frontend-Developer and I am well experienced with HTML, CSS, Javascript, TypeScript, Angular, NodeJS, Express, MongoDB.
+              Though I know a few Backend technologies I choose to be a Frontend-Developer.`,
+  },
+  {
+    name: 'Python Programming',
+    desc: `
+              I am a python programmer with intermidiate experience in it.
+              Also I have a little bit knowledge of Machine Learning and Deep Learning.
+              `,
+  },
+];
